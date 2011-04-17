928cd33b8b93b85b32b09b387d78306bf94f0b4e
Hallvard Nygard <hallvard.nygard@gmail.com>
2011-04-17 20:42:36 +0200

    Adding account name in autoimport list

--- project/views/bankaccount/autoimport/index.php
+++ project/views/bankaccount/autoimport/index.php
@@ -5,6 +5,8 @@ echo html::anchor('index.php/bankaccount_autoimport/create', __('Create new auto
 echo '<ul>';
 foreach($bankaccount_autoimports as $bankaccount_autoimport)
 {
+	$account = Sprig::factory('account', 
+		array('id' => $bankaccount_autoimport->account_id))->load();
 	echo '	<li>'.
 			'<b>'.$bankaccount_autoimport->id.':</b> '.
 			
@@ -17,7 +19,10 @@ foreach($bankaccount_autoimports as $bankaccount_autoimport)
 				'index.php/bankaccount_autoimport/delete/'.$bankaccount_autoimport->id,
 				__('Delete')
 			).
-			' - '.
+			' - ';
+	if($account->loaded())
+		echo '<b>'.$account->name.':</b> ';
+	echo
 			$bankaccount_autoimport->type.' - '.
 			$bankaccount_autoimport->text.
 		'</li>';
