89084d802cca4364588b610cb6accc5926182f71
Hallvard Nygard <hallvard.nygard@gmail.com>
2011-04-29 22:36:24 +0200

    Making bankaccount_autoimport list into a table, also adding the new options

--- project/views/bankaccount/autoimport/index.php
+++ project/views/bankaccount/autoimport/index.php
@@ -2,14 +2,37 @@
 
 echo html::anchor('index.php/bankaccount_autoimport/create', __('Create new autoimport'));
 
-echo '<ul>';
+
+echo '<table>'.chr(10);
+echo
+	'	<tr>'.chr(10).
+	'		<th>'.__('Id').'</th>'.chr(10).
+	'		<th>'.__('Options').'</th>'.chr(10).
+	'		<th>'.__('Account name').'</th>'.chr(10).
+	'		<th>'.__('Type').'</th>'.chr(10).
+	'		<th>'.__('Text').'</th>'.chr(10).
+	'		<th>'.__('Bankaccount').'</th>'.chr(10).
+	'		<th>'.__('Amount maximum').'</th>'.chr(10).
+	'		<th>'.__('Amount minimum').'</th>'.chr(10).
+	'		<th>'.__('Time maximum').'</th>'.chr(10).
+	'		<th>'.__('Time minimum').'</th>'.chr(10).
+	'	</tr>'.chr(10);
 foreach($bankaccount_autoimports as $bankaccount_autoimport)
 {
 	$account = Sprig::factory('account', 
 		array('id' => $bankaccount_autoimport->account_id))->load();
-	echo '	<li>'.
-			'<b>'.$bankaccount_autoimport->id.':</b> '.
+	if(!is_null($bankaccount_autoimport->bankaccount_id))
+	{
+		$bankaccount = Sprig::factory('bankaccount', 
+			array('id' => $bankaccount_autoimport->bankaccount_id))->load();
+		$bankaccount_num = $bankaccount->num;
+	}
+	else
+		$bankaccount_num = '&nbsp;';
+	echo '	<tr>'.
+			'<td>'.$bankaccount_autoimport->id.'</td> '.
 			
+			'<td>'.
 			html::anchor(
 				'index.php/bankaccount_autoimport/edit/'.$bankaccount_autoimport->id,
 				__('Edit')
@@ -19,12 +42,18 @@ foreach($bankaccount_autoimports as $bankaccount_autoimport)
 				'index.php/bankaccount_autoimport/delete/'.$bankaccount_autoimport->id,
 				__('Delete')
 			).
-			' - ';
-	if($account->loaded())
-		echo '<b>'.$account->name.':</b> ';
-	echo
-			$bankaccount_autoimport->type.' - '.
-			$bankaccount_autoimport->text.
-		'</li>';
+			'</td>'.
+			
+			'<td>'.$account->name.'</td>'.
+			
+			'<td>'.$bankaccount_autoimport->type.'</td>'.
+			'<td>'.$bankaccount_autoimport->text.'</td>'.
+			'<td>'.$bankaccount_num.'</td>'.
+			'<td>&nbsp;'.$bankaccount_autoimport->amount_max.'</td>'.
+			'<td>&nbsp;'.$bankaccount_autoimport->amount_min.'</td>'.
+			'<td>&nbsp;'.$bankaccount_autoimport->time_max.'</td>'.
+			'<td>&nbsp;'.$bankaccount_autoimport->time_min.'</td>'.
+		
+		'</tr>'.chr(10);
 }
-echo '</ul>';
+echo '</table>';
