52d120ae7aee962249f3985b8db2d43918b533d1
Hallvard Nygard <hallvard.nygard@gmail.com>
2011-09-04 17:24:30 +0200

    Adding one more optional parameter to route

--- project/bootstrap.php
+++ project/bootstrap.php
@@ -113,7 +113,7 @@ Kohana::modules(array(
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
-Route::set('default', '(<controller>(/<action>(/<id>(/<id2>(/<id3>(/<id4>))))))')
+Route::set('default', '(<controller>(/<action>(/<id>(/<id2>(/<id3>(/<id4>(/<id5>)))))))')
 	->defaults(array(
 		'controller' => 'welcome',
 		'action'     => 'index',
